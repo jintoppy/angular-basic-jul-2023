a01973ea6e7aee87f61863f3ee78aa4ddd3dceff
Jinto Jose <jintoppy@gmail.com>
2023-07-28 14:23:18 +0530

    service api call

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from './types';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +12,18 @@ export class AppService {
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.url);
+    const obs1 = this.http.get<User[]>(this.url);
+    const obs2 = obs1.pipe(
+      map((users) => {
+        return users.map((u) => {
+          return {
+            ...u,
+            name: `${u.id}: ${u.name}`,
+          };
+        });
+      })
+    );
+
+    return obs2;
   }
 }
